8ae94e29d9a851e1ceefd5f746afbb5cf660d8d2
Claire0223 <1327686271@qq.com>
2020-04-03 14:58:28 +0800

    practice1

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+#计算器类
+class Count:
+    def __init__(self,a,b):
+        self.a=int(a)
+        self.b=int(b)
+
+#加法
+    def add(self):
+        return self.a+self.b
+
+ #减法   
+    def sub(self):
+        return self.a-self.b
+
+        
